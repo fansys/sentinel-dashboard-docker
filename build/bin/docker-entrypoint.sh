1d2f5b5fee47d22a6f073b8f96a500f7225d9ea5
Cheney <cheney256@163.com>
2020-05-27 11:42:21 +0800

    use local jar, add sentinel auth environments

--- build/bin/docker-entrypoint.sh
+++ build/bin/docker-entrypoint.sh
@@ -6,10 +6,14 @@ RUN_CMD="java"
 RUN_CMD="$RUN_CMD -Dserver.port:\"$SERVER_PORT\""
 RUN_CMD="$RUN_CMD -Dcsp.sentinel.dashboard.server=\"$DASHBOARD_SERVER\""
 RUN_CMD="$RUN_CMD -Dproject.name=\"$PROJECT_NAME\""
+RUN_CMD="$RUN_CMD -Dauth.enabled=\"$AUTH_ENABLED\""
+RUN_CMD="$RUN_CMD -Dsentinel.dashboard.auth.username=\"$AUTH_USERNAME\""
+RUN_CMD="$RUN_CMD -Dsentinel.dashboard.auth.password=\"$AUTH_PASSWORD\""
+RUN_CMD="$RUN_CMD -Dserver.servlet.session.cookie.name=\"$AUTH_COOKIE_NAME\""
 
 RUN_CMD="$RUN_CMD $JAVA_OPTS"
 RUN_CMD="$RUN_CMD -jar"
-RUN_CMD="$RUN_CMD sentinel-dashboard-1.6.3.jar"
+RUN_CMD="$RUN_CMD sentinel-dashboard.jar"
 
 echo $RUN_CMD
 eval $RUN_CMD
